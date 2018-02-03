e9e566aaa99938a66c3d2076c70f924198b7dbba
Prakhar Agarwal <pkhrag@iitk.ac.in>
2018-02-04 02:11:54 +0530

    added symbol table

--- assign2/codegen.py
+++ assign2/codegen.py
@@ -1,14 +1,26 @@
 import input_ir
+import blocks
+import symbol
 
+
+# Corresponds to 4 operand instructions
 type_4 = ['+', '-', 'x', '/', '%', '&', '|', '^', '<<', '>>', '==', '<', '>', '!=', '<=', '>=']
 
+# Corresponds to 3 operand instructions
 type_3 = ['=', '!', '+=', '-=', 'x=', '/=', '%=', '&=', '|=', '^=', '<<=', '>>=', '*', 'ifgoto']
 
+# Corresponds to 2 operand instructions
 type_2 = ['++', '--', 'label', 'print', 'scan', 'call', 'goto']
 
+# Corresponds to 1 operand instructions
 type_1 = ['ret']
 
 instr_types = type_4 + type_3 + type_2 + type_1
+
+# Symbol Table object
+ST = symbol.symbolTable()
+
+# Creates IR table
 
 
 class instruction3AC:
@@ -16,27 +28,41 @@ class instruction3AC:
         insType = instr[1]
 
         if not(insType in instr_types):
-            raise ValueError('Instruction "' + insType + '" not defined')
+            raise ValueError('Instruction ' + insType + ' not defined')
 
         self.type = insType
 
         if insType in type_4:
             if len(instr) != 5:
-                raise ValueError('Inappropriate number of operands')
+                raise ValueError('Inappropriate number of operands in ' +
+                                 ', '.join(i for i in instr))
             self.dst = instr[2]
+
+            ST.insert(self.dst, "int")
+
             self.src1 = instr[3]
             self.src2 = instr[4]
 
         elif insType in type_3:
             if len(instr) != 4:
-                raise ValueError('Inappropriate number of operands')
+                raise ValueError('Inappropriate number of operands in ' +
+                                 ', '.join(i for i in instr))
             self.dst = instr[2]
+
+            if (self.type != 'ifgoto'):
+                ST.insert(self.dst, "int")
+
             self.src1 = instr[3]
 
         elif insType in type_2:
             if len(instr) != 3:
-                raise ValueError('Inappropriate number of operands')
+                raise ValueError('Inappropriate number of operands in ' +
+                                 ', '.join(i for i in instr))
+
             self.dst = instr[2]
+
+            if (self.type == 'label'):
+                ST.insert(self.dst, "void")
 
 
 # Use input function made by dutta
@@ -45,4 +71,10 @@ raw_ir = input_ir.dutta_input()
 ir = []
 
 for i in raw_ir:
-    ir.append(Instruction3AC(i))
+    ir.append(instruction3AC(i))
+
+bbl = blocks.findBlocks(ir)
+
+print ir
+print bbl
+print ST.table
