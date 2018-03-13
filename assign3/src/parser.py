ffa09afdc558446a9d350f72303dc07a9e5a5422
AkashKrDutta <akdboltzmann@gmail.com>
2018-03-14 02:17:38 +0530

    some lines

--- assign3/src/parser.py
+++ assign3/src/parser.py
@@ -5,91 +5,148 @@ from lexer import tokens, data
 from pprint import pprint
 
 
-# def p_expression_plus(p):
-#     'expression : expression PLUS term'
-#     p[0] = p[1] + p[3]
 
-# def p_star(p):
-#     '''start : PackageClause SEMICOLON ImportDeclRep TopLevelDeclRep'''
-#     p[0] = "<START> <PackageClause>" + p[1] + "</PackageClause>" + "<SEMICOLON/> <ImportDecl>" + str(p[3]) + "</ImportDecl> <SEMICOLON/> </START>"
+# ----------------  START --------------------------------
+def p_start(p):
+    '''start : PackageClause SEMICOLON ImportDeclRep'''
+    p[0] = ["start", p[1], ";", p[3]]
+# --------------------------------------------------------
 
-# def p_import_decl_rep(p):
-# 	'''ImportDeclRep : ImportDeclRep ImportDecl 
-# 					 | epsilon'''
-# 	if len(p) == 3:
-# 		if p[1]:
-# 			p[0] = p[1] + p[2]
-# 		else:
-# 			p[0] = p[2]
 
-# def p_package_clause(p):
-#     '''PackageClause : PACKAGE PackageName'''
-#     p[0] = "<PACKAGE/>" + "<PackageName>" + p[2] + "</PackageName>"
 
-# def p_package_name(p):
-#     '''PackageName : IDENTIFIER '''
-#     p[0] = "<IDENTIFIER>" + p[1] + "</IDENTIFIER>"
 
-# def p_import_decl(p):
-# 	'''ImportDecl : IMPORT SEMICOLON'''
-# 	p[0] = "<IMPORT/> <SEMICOLON/>"
+# ---------- PACKAGE CLAUSE --------------------
+def p_package_clause(p):
+    '''PackageClause : PACKAGE PackageName'''
+    p[0] = ["PackageClause", "package", p[2]]
+
+def p_package_name(p):
+    '''PackageName : IDENTIFIER'''
+    p[0] = ["PackageName", p[1]]
+# -----------------------------------------------
+
+
+
+
+# --------- IMPORT DECLARATIONS ---------------
+def p_import_decl_rep(p):
+	'''ImportDeclRep : ImportDeclRep ImportDecl
+					 | epsilon'''
+	if len(p) == 3:
+		p[0] = ["ImportDeclRep", p[1], p[2]]
+	else:
+		p[0] = ["ImportDeclRep", p[1]]
+		
+def p_import_decl(p):
+	'''ImportDecl : IMPORT ImportSpec 
+				  | IMPORT LPAREN ImportSpecRep RPAREN'''
+	if len(p) == 3:
+		p[0] = ["ImportDecl", "import", ";"]
+	else:
+		p[0] = ["ImportDecl", "import", "(", p[3], ")"]
+		
+
+
+def p_import_spec_rep(p):
+	''' ImportSpecRep : ImportSpecRep ImportSpec
+					  | epsilon '''
+	if len(p) == 3:
+		p[0] = ["ImportSpecRep", p[1], p[2]]
+	else:
+		p[0] = ["ImportSpecRep", p[1]]
+
+def p_import_spec(p):
+	''' ImportSpec : PackageNameDotOpt ImportPath '''
+	p[0] = ["ImportSpec", p[1], p[2]]
+
+def p_package_name_dot_opt(p):
+	''' PackageNameDotOpt : DOT 
+	                      | PackageName'''
+	if p[1]:
+		p[0] = ["PackageNameDotOpt", p[1]]
+	else :
+		p[0] = ["PackageNameDotOpt", "."]
+
+def p_import_path(p):
+	''' ImportPath : STRING '''
+	p[0] = ["ImportPath", p[1]]
+
+# -------------------------------------------------------
+
+
 
 # def p_toplevel_decl_rep(p):
-# 	'''TopLevelDeclRep : epsilon 
-# 					   | TopLevelDeclRep TopLevelDecl'''
+# 	'''TopLevelDeclRep : TopLevelDeclRep TopLevelDecl 
+# 					   | epsilon'''
+# 	if len(p) == 3:
+# 		p[0] = ["TopLevelDecl", p[1], p[2]]
+# 	else:
+# 		p[0] = ["TopLevelDecl", p[1]]
 
 # def p_toplevel_decl(p):
 # 	'''TopLevelDecl : epsilon'''
 
-# def p_empty(p):
-# 	'''epsilon :'''
+
+
+def p_empty(p):
+	'''epsilon : '''
+	p[0] = "epsilon"
 
 # def p_import_decl(p):
 
-def p_start(p):
-	'''start : expression'''
-	# p[0] = "<start>" + p[1] + "</start>"
-	p[0] = ['start', p[1]]
 
-def p_expression_plus(p):
-    '''expression : expression PLUS term
-                  | expression MINUS term'''
-    if p[2] == '+':
-        # p[0] = "<expr>" + p[1] + "</expr> + " + p[3]
-        p[0] = ["expression", p[1], '+', p[3]]
-    else:
-        # p[0] = "<expr>" + p[1] + "</expr> - " + p[3]
-        p[0] = ["expression", p[1], '-', p[3]]
-        # p[0] = p[1] - p[3]
-# def p_expression_minus(p):
-#     'expression : '
 
-def p_expression_term(p):
-    'expression : term'
-    # p[0] = "<term>" + p[1] + "</term>"
-    p[0] = ["expression", p[1]]
 
-def p_term_times(p):
-    'term : term STAR factor'
-    # p[0] = "<term>" + p[1] + "</term> * " + "<factor>" + p[3] + "</factor>"
-    p[0] = ["term", p[1], "*", p[3]]
 
-# def p_term_div(p):
-#     'term : term DIVIDE factor'
-#     p[0] = p[1] / p[3]
 
-def p_term_factor(p):
-    'term : factor'
-    p[0] = ["term", p[1]]
 
-def p_factor_num(p):
-    'factor : INTEGER'
-    # p[0] = str(p[1])
-    p[0] = ["factor", str(p[1])]
 
-# def p_factor_expr(p):
-#     'factor : LPAREN expression RPAREN'
-#     p[0] = p[2]
+# def p_start(p):
+# 	'''start : expression'''
+# 	# p[0] = "<start>" + p[1] + "</start>"
+# 	p[0] = ['start', p[1]]
+
+# def p_expression_plus(p):
+#     '''expression : expression PLUS term
+#                   | expression MINUS term'''
+#     if p[2] == '+':
+#         # p[0] = "<expr>" + p[1] + "</expr> + " + p[3]
+#         p[0] = ["expression", p[1], '+', p[3]]
+#     else:
+#         # p[0] = "<expr>" + p[1] + "</expr> - " + p[3]
+#         p[0] = ["expression", p[1], '-', p[3]]
+#         # p[0] = p[1] - p[3]
+# # def p_expression_minus(p):
+# #     'expression : '
+
+# def p_expression_term(p):
+#     'expression : term'
+#     # p[0] = "<term>" + p[1] + "</term>"
+#     p[0] = ["expression", p[1]]
+
+# def p_term_times(p):
+#     'term : term STAR factor'
+#     # p[0] = "<term>" + p[1] + "</term> * " + "<factor>" + p[3] + "</factor>"
+#     p[0] = ["term", p[1], "*", p[3]]
+
+
+
+# # def p_term_div(p):
+# #     'term : term DIVIDE factor'
+# #     p[0] = p[1] / p[3]
+
+# def p_term_factor(p):
+#     'term : factor'
+#     p[0] = ["term", p[1]]
+
+# def p_factor_num(p):
+#     'factor : INTEGER'
+#     # p[0] = str(p[1])
+#     p[0] = ["factor", str(p[1])]
+
+# # def p_factor_expr(p):
+# #     'factor : LPAREN expression RPAREN'
+# #     p[0] = p[2]
 
 
 
@@ -111,6 +168,4 @@ except EOFError:
 if not s:
   print("bas kar")
 result = parser.parse(s)
-# with open("tree.xml", "w") as f:
-	# f.write(result)
 print(result)
--- assign3/src/parsetab.py
+++ assign3/src/parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'AND AND_ASSIGN AND_XOR AND_XOR_ASSIGN ASSIGN BREAK CASE COLON COMMA CONST CONTINUE DECR DEFAULT DIVIDE DIVIDE_ASSIGN DOT ELSE EQUALS FLOAT FOR FUNC GOTO GREATER HEX IDENTIFIER IF IMAGINARY IMPORT INCR INTEGER LCURL LESSER LESS_EQUALS LOGICAL_AND LOGICAL_OR LPAREN LSHIFT LSHIFT_ASSIGN LSQUARE MINUS MINUS_ASSIGN MOD MOD_ASSIGN MORE_EQUALS NOT NOT_ASSIGN OCTAL OR OR_ASSIGN PACKAGE PLUS PLUS_ASSIGN QUICK_ASSIGN RANGE RCURL RETURN RPAREN RSHIFT RSHIFT_ASSIGN RSQUARE RUNE SEMICOLON STAR STAR_ASSIGN STRING STRUCT SWITCH TYPE VAR XOR XOR_ASSIGNstart : expressionexpression : expression PLUS term\n                  | expression MINUS termexpression : termterm : term STAR factorterm : factorfactor : INTEGER'
+_lr_signature = 'AND AND_ASSIGN AND_XOR AND_XOR_ASSIGN ASSIGN BREAK CASE COLON COMMA CONST CONTINUE DECR DEFAULT DIVIDE DIVIDE_ASSIGN DOT ELSE EQUALS FLOAT FOR FUNC GOTO GREATER HEX IDENTIFIER IF IMAGINARY IMPORT INCR INTEGER LCURL LESSER LESS_EQUALS LOGICAL_AND LOGICAL_OR LPAREN LSHIFT LSHIFT_ASSIGN LSQUARE MINUS MINUS_ASSIGN MOD MOD_ASSIGN MORE_EQUALS NOT NOT_ASSIGN OCTAL OR OR_ASSIGN PACKAGE PLUS PLUS_ASSIGN QUICK_ASSIGN RANGE RCURL RETURN RPAREN RSHIFT RSHIFT_ASSIGN RSQUARE RUNE SEMICOLON STAR STAR_ASSIGN STRING STRUCT SWITCH TYPE VAR XOR XOR_ASSIGNstart : PackageClause SEMICOLON ImportDeclRep TopLevelDeclRepPackageClause : PACKAGE PackageNamePackageName : IDENTIFIERImportDeclRep : ImportDeclRep ImportDecl\n\t\t\t\t\t | epsilonImportDecl : IMPORT ImportSpec \n\t\t\t\t  | IMPORT LPAREN ImportSpecRep RPAREN ImportSpecRep : ImportSpecRep ImportSpec\n\t\t\t\t\t  | epsilon  ImportSpec : PackageNameDotOpt ImportPath  PackageNameDotOpt : DOT \n\t                      | PackageName ImportPath : STRING TopLevelDeclRep : TopLevelDeclRep TopLevelDecl \n\t\t\t\t\t   | epsilonTopLevelDecl : epsilonepsilon : '
     
-_lr_action_items = {'INTEGER':([0,6,7,8,],[4,4,4,4,]),'$end':([1,2,3,4,5,9,10,11,],[-4,0,-6,-7,-1,-5,-2,-3,]),'STAR':([1,3,4,9,10,11,],[6,-6,-7,-5,6,6,]),'MINUS':([1,3,4,5,9,10,11,],[-4,-6,-7,8,-5,-2,-3,]),'PLUS':([1,3,4,5,9,10,11,],[-4,-6,-7,7,-5,-2,-3,]),}
+_lr_action_items = {'RPAREN':([15,20,21,22,23,25,],[-17,-9,24,-10,-13,-8,]),'STRING':([6,17,18,19,],[-3,-12,23,-11,]),'SEMICOLON':([1,5,6,],[4,-2,-3,]),'PACKAGE':([0,],[2,]),'LPAREN':([12,],[15,]),'IMPORT':([4,7,8,11,16,22,23,24,],[-17,-5,12,-4,-6,-10,-13,-7,]),'IDENTIFIER':([2,12,15,20,21,22,23,25,],[6,6,-17,-9,6,-10,-13,-8,]),'DOT':([12,15,20,21,22,23,25,],[19,-17,-9,19,-10,-13,-8,]),'$end':([3,4,7,8,9,10,11,13,14,16,22,23,24,],[0,-17,-5,-17,-1,-15,-4,-16,-14,-6,-10,-13,-7,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'start':([0,],[2,]),'term':([0,7,8,],[1,10,11,]),'expression':([0,],[5,]),'factor':([0,6,7,8,],[3,9,3,3,]),}
+_lr_goto_items = {'TopLevelDeclRep':([8,],[9,]),'PackageClause':([0,],[1,]),'TopLevelDecl':([9,],[14,]),'ImportSpec':([12,21,],[16,25,]),'epsilon':([4,8,9,15,],[7,10,13,20,]),'PackageName':([2,12,21,],[5,17,17,]),'ImportDecl':([8,],[11,]),'ImportPath':([18,],[22,]),'start':([0,],[3,]),'ImportSpecRep':([15,],[21,]),'PackageNameDotOpt':([12,21,],[18,18,]),'ImportDeclRep':([4,],[8,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,11 +27,21 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> start","S'",1,None,None,None),
-  ('start -> expression','start',1,'p_start','parser.py',50),
-  ('expression -> expression PLUS term','expression',3,'p_expression_plus','parser.py',55),
-  ('expression -> expression MINUS term','expression',3,'p_expression_plus','parser.py',56),
-  ('expression -> term','expression',1,'p_expression_term','parser.py',68),
-  ('term -> term STAR factor','term',3,'p_term_times','parser.py',73),
-  ('term -> factor','term',1,'p_term_factor','parser.py',82),
-  ('factor -> INTEGER','factor',1,'p_factor_num','parser.py',86),
+  ('start -> PackageClause SEMICOLON ImportDeclRep TopLevelDeclRep','start',4,'p_start','parser.py',11),
+  ('PackageClause -> PACKAGE PackageName','PackageClause',2,'p_package_clause','parser.py',16),
+  ('PackageName -> IDENTIFIER','PackageName',1,'p_package_name','parser.py',20),
+  ('ImportDeclRep -> ImportDeclRep ImportDecl','ImportDeclRep',2,'p_import_decl_rep','parser.py',26),
+  ('ImportDeclRep -> epsilon','ImportDeclRep',1,'p_import_decl_rep','parser.py',27),
+  ('ImportDecl -> IMPORT ImportSpec','ImportDecl',2,'p_import_decl','parser.py',34),
+  ('ImportDecl -> IMPORT LPAREN ImportSpecRep RPAREN','ImportDecl',4,'p_import_decl','parser.py',35),
+  ('ImportSpecRep -> ImportSpecRep ImportSpec','ImportSpecRep',2,'p_import_spec_rep','parser.py',42),
+  ('ImportSpecRep -> epsilon','ImportSpecRep',1,'p_import_spec_rep','parser.py',43),
+  ('ImportSpec -> PackageNameDotOpt ImportPath','ImportSpec',2,'p_import_spec','parser.py',50),
+  ('PackageNameDotOpt -> DOT','PackageNameDotOpt',1,'p_package_name_dot_opt','parser.py',54),
+  ('PackageNameDotOpt -> PackageName','PackageNameDotOpt',1,'p_package_name_dot_opt','parser.py',55),
+  ('ImportPath -> STRING','ImportPath',1,'p_import_path','parser.py',62),
+  ('TopLevelDeclRep -> TopLevelDeclRep TopLevelDecl','TopLevelDeclRep',2,'p_toplevel_decl_rep','parser.py',68),
+  ('TopLevelDeclRep -> epsilon','TopLevelDeclRep',1,'p_toplevel_decl_rep','parser.py',69),
+  ('TopLevelDecl -> epsilon','TopLevelDecl',1,'p_toplevel_decl','parser.py',76),
+  ('epsilon -> <empty>','epsilon',0,'p_empty','parser.py',79),
 ]
