fd8273454ed2ebcf10f35a7cf4b2c45dbf1c2b6a
swarnadeep97 <swarnadeep97@gmail.com>
2018-04-04 18:56:33 +0530

    Added makefile and readme

---
+++ assign4/test/lexer_go_test/dangling_if_else.go
@@ -0,0 +1,20 @@
+package main;
+import "fmt";
+func main() {
+	var i int_t = 0;
+	var a[3] int_t;
+	a[0] = 1;
+	a[1] = 2;
+	a[2] = 3;
+	
+	if i<=3 {
+		a[i]++;
+	};
+		
+	if i>=2 {
+		a[i]--;
+	} else{
+		a[i]=1;
+	};
+
+};
---
+++ assign4/test/lexer_go_test/for_go.go
@@ -0,0 +1,16 @@
+package main;
+
+import (
+	"fmt";
+);
+
+func main() {
+	var i int_t = 6;
+	for ; i <= 8 && i>=6 && i!=7; i++ {
+		if i>=0 {
+			fmt.Println("yes");
+		} else {
+			fmt.Println("no");
+		};
+	};
+};
