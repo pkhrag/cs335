8a288bde2a783357a968043d090131aa2606b6a1
AkashKrDutta <akdboltzmann@gmail.com>
2018-04-08 03:54:46 +0530

    added test file

---
+++ assign4/src/test3.go
@@ -0,0 +1,17 @@
+package maie;
+
+import "fmt";
+
+type testStruct struct {
+	i int_t;
+	j  int_t;
+
+};
+
+type myfloat float_t;
+
+func main() {
+	var testStruct type testStruct = 4;
+	var  i type myfloat;
+	var t int_t = 4;
+};
